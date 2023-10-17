47d0ba716a0c445e8a09de4e9895cf01dd575adc
Marilia Morais <mmjanizelli@gmail.com>
2023-10-16 22:22:46 -0300

    [chore] Add initial models and migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid 
 
-# Create your models here.
+class Cupcake(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sabor = models.CharField(max_length=100)
+    cobertura = models.TextField()
+    recheio = models.TextField()
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sabor} Cupcake com Cobertura de {self.cobertura} e Recheio de {self.recheio}"
+
+class Usuario(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sabor = models.CharField(max_length=100)
+    cobertura = models.TextField()
+    recheio = models.TextField()
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 

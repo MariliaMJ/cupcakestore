785b0e12b63f420cda3fef390b15b564d11d9f01
Marilia Morais <mmjanizelli@gmail.com>
2023-10-22 22:21:42 -0300

    [feat] Add cupcake listing feature

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Cupcake
 
+def get_cupcakes(request):
+    cupcakes = Cupcake.objects.all()
+    return render(request, 'list.html', {'cupcakes': cupcakes})
 # Create your views here.
